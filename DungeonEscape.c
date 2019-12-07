0e2d7f0c0dfef9f35792267ad88d9e667af66334
PhanTom-Foto <52395165+PhanTom-Foto@users.noreply.github.com>
2019-12-08 00:50:41 +0700

    Update DungeonEscape.c

--- DungeonEscape.c
+++ DungeonEscape.c
@@ -1,65 +1,120 @@
+//******************************************************//
+//  Created by DonaldTram on 12/7/19.                  //
+//  Copyright © 2019 DonaldTr4n. All rights reserved. //
+//***************************************************//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 int pX,pY,kX,kY,eX,eY;
+int check=0;
 char map[4][4];
+char step;
 
-void p_move()
+void p_move(char step) //di chuyển bằng các phím 'w a s d'
 {
-    char step;
-    printf("Nhap buoc di cua ban: ");
-    scanf("%c",&step);
     if (step == 'w')
     {
-        if(pY -1 <0) printf("Hay thu di huong khac!\n");
+        if(pY -1 <0) printf("Hay thu di huong khac!");
         else
         {
             map[pY][pX]='-';
-            pY--;
+            pY--;// w là hướng lên trên nên trục Y trong mảng sẽ giảm
             map[pY][pX]='P';
-            
         }
     }
     
     if (step == 's')
     {
-        if(pY +1 >4) printf("Hay thu di huong khac!\n");
+        if(pY +1 >3) printf("Hay thu di huong khac!");
         else
         {
             map[pY][pX]='-';
-            pY++;
+            pY++;// s hướng xuống dưới nên trục Y trong mảng sẽ tăng
             map[pY][pX]='P';
-            
         }
     }
     
     if (step == 'a')
     {
-        if(pX -1<0) printf("Hay thu di huong khac!\n");
+        if(pX -1<0) printf("Hay thu di huong khac! ");
         else
         {
             map[pY][pX]='-';
-            pX--;
+            pX--;// a là hướng trái nên trục X trong mảng sẽ giảm
             map[pY][pX]='P';
-            
         }
     }
     
     if (step == 'd')
     {
-        if(pX +1 >4) printf("Hay thu di huong khac!\n");
+        if(pX +1 >3) printf("Hay thu di huong khac! ");
         else
         {
             map[pY][pX]='-';
+            pX++;// d là hướng trái nên trục X trong mảng sẽ tăng
+            map[pY][pX]='P';
+        }
+    }
+    if (map[pY][pX] == map[kY][kX]) //kiểm tra đã lấy chìa khóa hay chưa
+    {
+        check=1;
+        printf("Da tim thay chia khoa! ");
+    }
+    else check=0;
+    
+}
+
+void p_return(char step) // CTCon lặp lại, CTC này ngược so với CTC @p_move
+{
+    if (step == 'w')
+    {
+        if(pY +1 >3) printf("error");
+        else
+        {
+            map[pY][pX]='E';
+            pY++;
+            map[pY][pX]='P';
+        }
+    }
+    
+    if (step == 's')
+    {
+        if(pY -1 <0) printf("error");
+        else
+        {
+            map[pY][pX]='E';
+            pY--;
+            map[pY][pX]='P';
+        }
+    }
+    
+    if (step == 'a')
+    {
+        
+        if(pX +1 >3) printf("error");
+        else
+        {
+            map[pY][pX]='E';
             pX++;
             map[pY][pX]='P';
-            
+        }
+    }
+    
+    if (step == 'd')
+    {
+        if(pX -1<0) printf("error");
+        else
+        {
+            map[pY][pX]='E';
+            pX--;
+            map[pY][pX]='P';
         }
     }
 }
 
-void map_move()
+void map_move() // CTC in ra map sau khi @p_move
 {
     for(int i=0;i<4;i++)
     {
@@ -71,6 +126,24 @@ void map_move()
     }
 }
 
+void vonglap() //lặp lại khi tìm được lối ra nhưng chưa có chìa khóa
+{
+    do{
+        map_move();
+        printf("\nNhap buoc di cua ban: \n");
+        scanf("%c",&step);
+        fflush(stdin);
+        p_move(step);
+        
+    }while(map[pY][pX] != map[eY][eX]);
+}
+
+
+
+//******************************************************//
+//  Created by DonaldTram on 12/7/19.                  //
+//  Copyright © 2019 DonaldTr4n. All rights reserved. //
+//***************************************************//
 int main(void)
 {
     int m=4;
@@ -100,18 +173,16 @@ int main(void)
     } while (map[eY][eX] != '-');
     map[eY][eX]='E';
     
-    map_move();
+    //map_move();
     
-    do{
-        p_move();
-        printf("\n");
-        map_move();
-        if(map[pY][pX] == map[eY][eX])
-        {
-            printf("Da tim chia khoa");
-            continue;
-        }
-    } while(map[pY][pX] != map[kY][kX]);
-    printf("Tim thay lopi ra nhung chua co chia khoa");
+    vonglap();
+    if(check==1) printf("Da tim thay loi ra!\n");
+    else if(check==0)
+    {
+        printf("Tim thay loi ra nhung chua co chia khoa\n");
+        printf("\nNhap buoc di cua ban: \n");
+        p_return(step);
+        vonglap();
+    }
     
 }
